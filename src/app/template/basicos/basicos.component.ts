d34b0b01bdcbf3311dffcbd96ac4f88b3657b4e6
Pablo Romero <pabloromero@MacBook-Air-de-Pablo.local>
2021-11-10 12:54:15 -0300

    validaciones en el formulario agregadas

--- src/app/template/basicos/basicos.component.ts
+++ src/app/template/basicos/basicos.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit, ViewChild } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-basicos',
@@ -8,9 +9,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class BasicosComponent implements OnInit {
 
+  @ViewChild('miFormulario') miFormulario!: NgForm;
+
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  guardar( ) {
+    console.log(this.miFormulario); 
+  }
+
+  nombreValido(): boolean {
+    return this.miFormulario?.controls.producto?.invalid 
+    && this.miFormulario?.controls.producto?.touched;
   }
 
 }
