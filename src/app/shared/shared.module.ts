029fc9dff7f75e73808ccede249cd07e89ffef24
Pablo Romero <pabloromero@MacBook-Air-de-Pablo.local>
2021-11-10 11:53:26 -0300

    estructura de formularios agregada

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
 import { SidemenuComponent } from './sidemenu/sidemenu.component';
 
 
@@ -9,7 +10,8 @@ import { SidemenuComponent } from './sidemenu/sidemenu.component';
     SidemenuComponent
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    RouterModule
   ],
   exports: [
     SidemenuComponent
